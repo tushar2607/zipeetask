56d2790fde8de6a134bdb61fb87071bf874940a6
tushar2607 <68341729+tushar2607@users.noreply.github.com>
2025-07-09 10:57:07 +0530

    remaining files

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Manager API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
